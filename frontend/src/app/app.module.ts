b8ff721724fdff92615124f9147c4a9eb9a85758
Eirik Joneid <eirijomine@gmail.com>
2021-10-04 14:11:48 +0200

    Auth0 authentication added Auth0 library for angular. created auth-button component for login redirect.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthButtonComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-fwlq8v0n.eu.auth0.com',
+      clientId: 'UhMx2hMd70OxMqZZFhZZctAIWRuVvaA2'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
