89daa8b2bf13c675c6f581e9806f8cde2fb154cd
Jakob <jakob.walmann@outlook.com>
2021-10-05 12:52:52 +0200

    Deleted unused classes

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,23 +3,20 @@ import { CommonModule } from '@angular/common';
 import {HomePage} from "./pages/home.page";
 import {AuthButtonComponent} from "./components/auth-button/auth-button.component";
 import {LoginComponent} from "./components/login/login.component";
-import {ActiveGameComponent} from "./components/active-game/active-game.component";
 import {HomeRoutingModule} from "./home-routing.module";
 
 @NgModule({
   declarations: [
     HomePage,
     AuthButtonComponent,
-    LoginComponent,
-    ActiveGameComponent
+    LoginComponent
   ],
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
   exports: [
-    LoginComponent,
-    ActiveGameComponent
+    LoginComponent
   ]
 })
 export class HomeModule { }
