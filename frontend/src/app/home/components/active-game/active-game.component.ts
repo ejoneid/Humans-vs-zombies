c63c244c0b78d6c3b1f3060c5988a590773cfe63
Jakob <jakob.walmann@outlook.com>
2021-10-05 13:18:09 +0200

    Prettier design and created the proper class to hold active game previews in.

---
+++ frontend/src/app/home/components/active-game/active-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-game',
+  templateUrl: './active-game.component.html',
+  styleUrls: ['./active-game.component.css']
+})
+export class ActiveGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -4,12 +4,14 @@ import {HomePage} from "./pages/home.page";
 import {AuthButtonComponent} from "./components/auth-button/auth-button.component";
 import {LoginComponent} from "./components/login/login.component";
 import {HomeRoutingModule} from "./home-routing.module";
+import { ActiveGameComponent } from './components/active-game/active-game.component';
 
 @NgModule({
   declarations: [
     HomePage,
     AuthButtonComponent,
-    LoginComponent
+    LoginComponent,
+    ActiveGameComponent
   ],
   imports: [
     CommonModule,
