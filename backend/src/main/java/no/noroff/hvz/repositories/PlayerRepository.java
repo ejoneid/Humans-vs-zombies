e8d25edb7d96efd35fdae0f2efc0be84b1a4e541
fredrik rikheim <fredrik.rikheim@outlook.com>
2021-10-05 08:48:42 +0200

    FIX: fixed wrong import in repository interface

--- backend/src/main/java/no/noroff/hvz/repositories/PlayerRepository.java
+++ backend/src/main/java/no/noroff/hvz/repositories/PlayerRepository.java
@@ -1,6 +1,7 @@
 package no.noroff.hvz.repositories;
 
+import no.noroff.hvz.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayerRepository extends JpaRepository<PlayerRepository, Long> {
+public interface PlayerRepository extends JpaRepository<Player, Long> {
 }
