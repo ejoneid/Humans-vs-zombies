6b4b67ffc76c738ad6778d18562baad7c115d9d7
Jakob <jakob.walmann@outlook.com>
2021-10-06 11:06:43 +0200

    Google Maps properly implemented with dark mode, size is wrong and needs fixing

--- frontend/src/app/game-info/map/map.component.ts
+++ frontend/src/app/game-info/map/map.component.ts
@@ -1,14 +1,128 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {catchError, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  //Is defined from ngAfterViewInit()
+  @ViewChild("gmap") gmap: ElementRef | undefined;
 
-  ngOnInit(): void {
+  apiLoaded: Observable<boolean>;
+
+  options: google.maps.MapOptions = {
+    center: { lat: 40.674, lng: -73.945 },
+    zoom: 12,
+    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+  };
+
+  constructor(httpClient: HttpClient) {
+    //Maps API key: AIzaSyDLrbUDvEj78cTcTCheVdJbIH5IT5xPAkQ
+    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyDLrbUDvEj78cTcTCheVdJbIH5IT5xPAkQ', 'initMap')
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+      );
   }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    google.maps.event.trigger(map, "resize");
+    console.log(this.gmap);
+  }
+
+  initMap(): void {
+    // Styles a map in night mode.
+    new google.maps.Map(
+      document.getElementById("map") as HTMLElement,
+      this.options
+    );
+  }
+
 }
