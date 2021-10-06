9d15b3604c04a89bb7e6d88e81350fd2b8596994
fredrik rikheim <fredrik.rikheim@outlook.com>
2021-10-06 10:44:41 +0200

    FEAT: Changed GameController to use DTO

--- backend/src/main/java/no/noroff/hvz/controllers/GameController.java
+++ backend/src/main/java/no/noroff/hvz/controllers/GameController.java
@@ -1,14 +1,20 @@
 package no.noroff.hvz.controllers;
 
+import no.noroff.hvz.dto.GameDTO;
+import no.noroff.hvz.mapper.Mapper;
 import no.noroff.hvz.models.Game;
 import no.noroff.hvz.models.Message;
 import no.noroff.hvz.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/game")
@@ -17,33 +23,77 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @Autowired
+    private Mapper mapper;
+
     @GetMapping
-    public ResponseEntity<List<Game>> getAllGames() {
-        return gameService.getAllGames();
+    public ResponseEntity<List<GameDTO>> getAllGames() {
+        List<GameDTO> games = gameService.getAllGames().stream().map(mapper::toGameTDO).collect(Collectors.toList());
+        HttpStatus status = HttpStatus.OK;
+        return new ResponseEntity<>(games, status);
     }
 
     @GetMapping("{/id}")
-    public ResponseEntity<Game> getSpecificGame(@PathVariable Long id) {
-        return gameService.getSpecificGame(id);
+    public ResponseEntity<GameDTO> getSpecificGame(@PathVariable Long id) {
+        HttpStatus status;
+        Game game = gameService.getSpecificGame(id);
+        if(game.getId() == null) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        else {
+            status = HttpStatus.OK;
+        }
+        return new ResponseEntity<>(mapper.toGameTDO(game),status);
     }
 
     @PostMapping
-    public ResponseEntity<Game> createNewGame(@RequestBody Game game) {
-        return gameService.createNewGame(game);
+    public ResponseEntity<GameDTO> createNewGame(@RequestBody Game game) {
+        Game addedGame = gameService.createNewGame(game);
+        HttpStatus status = HttpStatus.CREATED;
+        return new ResponseEntity<>(mapper.toGameTDO(addedGame), status);
     }
 
     @PutMapping("{/id}")
-    public ResponseEntity<Game> updateSpecificGame(@PathVariable Long id, @RequestBody Game game) {
-        return gameService.updateSpecificGame(id, game);
+    public ResponseEntity<GameDTO> updateSpecificGame(@PathVariable Long id, @RequestBody Game game) {
+        HttpStatus status;
+        if(!Objects.equals(id,game.getId())) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(mapper.toGameTDO(new Game()),status);
+        }
+        Game updatedGame = gameService.updateSpecificGame(id, game);
+        if(updatedGame.getId() == null) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        else {
+            status = HttpStatus.OK;
+        }
+        return new ResponseEntity<>(mapper.toGameTDO(updatedGame),status);
     }
 
     @DeleteMapping("{/id}")
     public ResponseEntity<Game> deleteGame(@PathVariable Long id) {
-        return gameService.deleteGame(id);
+        HttpStatus status;
+        Game deletedGame = gameService.deleteGame(id);
+        if(deletedGame.getId() == null) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        else {
+            status = HttpStatus.OK;
+        }
+        return new ResponseEntity<>(deletedGame, status);
+
     }
 
     @GetMapping("{/id}/chat")
     public ResponseEntity<List<Message>> getGameChat(@PathVariable Long id) {
-        return gameService.getGameChat(id);
+        HttpStatus status;
+        List<Message> messages = gameService.getGameChat(id);
+        if( messages == null) {
+            status = HttpStatus.NOT_FOUND;
+        }
+        else {
+            status = HttpStatus.OK;
+        }
+        return new ResponseEntity<>(messages,status);
     }
 }
