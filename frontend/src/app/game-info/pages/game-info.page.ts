d4502728bf33794c183757f15e65f4b357ed5767
Jakob <jakob.walmann@outlook.com>
2021-10-06 14:13:55 +0200

    Game info now takes in parameters to update. Currently, mock data is used, since no info is fetched from the backend.

--- frontend/src/app/game-info/pages/game-info.page.ts
+++ frontend/src/app/game-info/pages/game-info.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+/*import {GameInfo} from "../../models/game-info.model";*/
 
 @Component({
   selector: 'app-game-info-page',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-info.page.css']
 })
 export class GameInfoPage implements OnInit {
+
+  // Should be set from a request to the backend in the constructor
+  /*gameInfo: GameInfo;*/
+
+  //Mock data for the gameInfo
+  mockDescription: string = "This game description contains a lot of information about how the game works and whatever special rules it has. The important part is that it is long enough to enable the scrolling function of the view ;)"
 
   constructor() { }
 
