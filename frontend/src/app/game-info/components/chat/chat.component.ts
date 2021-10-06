71d6d2dd321a7b91a2550aa03e14c6acc6da39b9
Jakob <jakob.walmann@outlook.com>
2021-10-06 14:41:53 +0200

    Messages now have their own component and are implemented into the chat in a scalable way. Fixed a small mistake with the project structure.

---
+++ frontend/src/app/game-info/components/chat/chat.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Message} from "../../../models/message.model";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  @Input()
+  public chatMessages: Message[] = [new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage(), new TestMessage()];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+//-----
+//Just for testing purposes
+class TestMessage implements Message {
+  chat = "global";
+  content = "This is a chat message that should cover more than one line please";
+  id = 3;
+  sender = "Test User";
+  squad = null;
+  time = "14:31";
+
+}
+//-----
