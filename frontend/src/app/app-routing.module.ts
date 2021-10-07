9857c1eedf401e31a206dd1e6487ee1825d73e1c
Eirik Joneid <eirijomine@gmail.com>
2021-10-07 10:33:18 +0200

    login fix removed redirect from "/ -> /home", becuase it messes up auth0 login.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,13 +5,9 @@ const routes: Routes = [
   { path: '',
     loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
   },
-  { path: 'home',
-    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
-  },
   { path: 'info',
     loadChildren: () => import("./game-info/game-info.module").then(m => m.GameInfoModule)
   },
-  // { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
