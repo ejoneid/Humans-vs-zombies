053a91cd14f689a243f416ba23dcd0deaa382e52
Jakob <jakob.walmann@outlook.com>
2021-10-07 14:26:27 +0200

    Created components

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AdminPage} from './pages/admin.page';
 import {AdminRoutingModule} from "./admin-routing.module";
+import { MapComponent } from './components/map/map.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { MissionComponent } from './components/mission/mission.component';
+import { GameComponent } from './components/game/game.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
-    AdminPage
+    AdminPage,
+    MapComponent,
+    ChatComponent,
+    MissionComponent,
+    GameComponent,
+    PlayerComponent
   ],
   imports: [
     CommonModule,
