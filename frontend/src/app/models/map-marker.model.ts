434b1645b63cf6eab423002479acb8782bcd68b6
Jakob <jakob.walmann@outlook.com>
2021-10-11 11:27:02 +0200

    Map information windows now open when a marker is clicked. The content is set to be the description of the mission/kill.

--- frontend/src/app/models/map-marker.model.ts
+++ frontend/src/app/models/map-marker.model.ts
@@ -1,11 +1,15 @@
 export interface MapMarker {
-  name: string,
   description: string | null,
   position: {
     lat: number,
     lng: number
   },
+  label: {
+    text: string,
+    color: string
+  },
   options: {
     icon: string
-  }
+  },
+  title: string
 }
