7f346ee89dd45e73f80c892832c1c387cd5e9f11
fredrik rikheim <fredrik.rikheim@outlook.com>
2021-10-15 14:49:35 +0200

    CHORE: added javadocs

--- backend/src/main/java/no/noroff/hvz/services/MissionService.java
+++ backend/src/main/java/no/noroff/hvz/services/MissionService.java
@@ -24,32 +24,56 @@ public class MissionService {
     @Autowired
     private GameRepository gameRepository;
 
+    /**
+     * Method for getting all missions in a game
+     * @param gameID ID of game
+     * @return List of missions in game
+     */
     public List<Mission> getAllMissions(Long gameID) {
         List<Mission> missions = null;
         if(gameRepository.existsById(gameID)) {
-            Game game = gameRepository.findById(gameID).get();
+            Game game = gameRepository.getById(gameID);
             missions = new ArrayList<>(game.getMissions());
         }
         return missions;
     }
 
+    /**
+     * Method for getting all missions for a faction in a game
+     * @param gameID ID of game
+     * @param isHuman true for human faction
+     * @return list of faction missions in agme
+     */
     public List<Mission> getAllMissionsFaction(Long gameID, boolean isHuman) {
         List<Mission> missions = null;
         if(gameRepository.existsById(gameID)) {
-            Game game = gameRepository.findById(gameID).get();
+            Game game = gameRepository.getById(gameID);
+            //filters and only gets the mission which match the faction
             missions = game.getMissions().stream().filter(mission -> Objects.equals(mission.isHuman(),isHuman)).collect(Collectors.toList());
         }
         return missions;
     }
 
+    /**
+     * Method for getting a mission
+     * @param gameID ID of game
+     * @param missionID ID of mission
+     * @return mission
+     */
     public Mission getSpecificMission(Long gameID, Long missionID) {
         Mission mission = null;
         if(missionRepository.existsById(missionID) && gameRepository.existsById(gameID)) {
-            mission = missionRepository.findById(missionID).get();
+            mission = missionRepository.getById(missionID);
         }
         return mission;
     }
 
+    /**
+     * Method for creating a new mission
+     * @param gameID ID of game
+     * @param mission DTO with the new missions contents
+     * @return the new mission
+     */
     public Mission createNewMission(Long gameID, Mission mission) {
         Mission addedMission = new Mission();
         if(gameRepository.existsById(gameID)) {
@@ -58,6 +82,13 @@ public class MissionService {
         return addedMission;
     }
 
+    /**
+     * Method for updating a mission
+     * @param gameID ID of game
+     * @param missionID ID of mission
+     * @param mission DTO with updated contents
+     * @return updated mission
+     */
     public Mission updateMission( Long gameID, Long missionID, Mission mission) {
         Mission updatedMission = new Mission();
         if(gameRepository.existsById(gameID) && missionRepository.existsById(missionID)) {
@@ -66,10 +97,16 @@ public class MissionService {
         return updatedMission;
     }
 
+    /**
+     * Method for deleting a mission
+     * @param gameID ID of game
+     * @param missionID ID of mission
+     * @return deleted mission
+     */
     public Mission deleteMission(Long gameID, Long missionID) {
         Mission deletedMission = new Mission();
         if(gameRepository.existsById(gameID) && missionRepository.existsById(missionID)) {
-            deletedMission = missionRepository.findById(missionID).get();
+            deletedMission = missionRepository.getById(missionID);
             missionRepository.deleteById(missionID);
         }
         return deletedMission;
