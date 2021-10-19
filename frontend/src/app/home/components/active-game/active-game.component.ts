8f8c083992f6954a6e4b83743187ae4e031e185a
Jakob <jakob.walmann@outlook.com>
2021-10-19 15:36:06 +0200

    404 is because admins currently are unable to go to a player game.

--- frontend/src/app/home/components/active-game/active-game.component.ts
+++ frontend/src/app/home/components/active-game/active-game.component.ts
@@ -44,7 +44,7 @@ export class ActiveGameComponent implements OnInit, OnChanges {
   toGameInfo(gameId: number, playerId: number | null): Promise<boolean> | void {
     if (playerId == null) {
       this.homeAPI.createPlayer(gameId)
-        .then(res => res.subscribe( //TODO: Fix the 404 here
+        .then(res => res.subscribe( //TODO: Fix the 404 here. Currently, an admin can't look at the info of a game.
           data => {
             playerId = data.id
             return this.router.navigate(["game/"+gameId+"/player/"+playerId]);
