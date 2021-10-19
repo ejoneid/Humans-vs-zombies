e057d97b767ec8f531c963326e01fc577767ccb7
fredrik rikheim <fredrik.rikheim@outlook.com>
2021-10-19 13:56:48 +0200

    FIX: Update mission did not map id, so new mission was created instead

--- backend/src/main/java/no/noroff/hvz/models/Mission.java
+++ backend/src/main/java/no/noroff/hvz/models/Mission.java
@@ -46,6 +46,18 @@ public class Mission {
         this.game = game;
     }
 
+    public Mission(Long id, String name, boolean isHuman, String description, Date startTime, Date endTime, String lat, String lng, Game game) {
+        this.id = id;
+        this.name = name;
+        this.isHuman = isHuman;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.lat = lat;
+        this.lng = lng;
+        this.game = game;
+    }
+
     public Mission(){}
 
     public Long getId() {
