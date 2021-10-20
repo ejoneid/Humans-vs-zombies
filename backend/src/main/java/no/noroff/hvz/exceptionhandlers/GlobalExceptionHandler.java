a081ea133799263a24d1b25d32cd706e053bdad4
fredrik rikheim <fredrik.rikheim@outlook.com>
2021-10-20 14:21:09 +0200

    FEAT: Added error messages response entities returned in exception handler

--- backend/src/main/java/no/noroff/hvz/exceptionhandlers/GlobalExceptionHandler.java
+++ backend/src/main/java/no/noroff/hvz/exceptionhandlers/GlobalExceptionHandler.java
@@ -18,55 +18,55 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NullPointerException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleNoSuchElementException(NullPointerException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handleNoSuchElementException(NullPointerException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidBiteCodeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> handleInvalidBiteCodeException(InvalidBiteCodeException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleInvalidBiteCodeException(InvalidBiteCodeException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AppUserNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Object> handleInvalidBiteCodeException(AppUserNotFoundException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleInvalidBiteCodeException(AppUserNotFoundException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MissingPermissionsException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<Object> handleInvalidBiteCodeException(MissingPermissionsException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    public ResponseEntity<String> handleInvalidBiteCodeException(MissingPermissionsException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(AppUserAlreadyExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<Object> handleInvalidBiteCodeException(AppUserAlreadyExistException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    public ResponseEntity<String> handleInvalidBiteCodeException(AppUserAlreadyExistException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(MissingPlayerException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleInvalidBiteCodeException(MissingPlayerException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handleInvalidBiteCodeException(MissingPlayerException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PlayerAlreadyExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ResponseEntity<Object> handleInvalidBiteCodeException(PlayerAlreadyExistException e, WebRequest r) {
-        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    public ResponseEntity<String> handleInvalidBiteCodeException(PlayerAlreadyExistException e, WebRequest r) {
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
     }
 }
